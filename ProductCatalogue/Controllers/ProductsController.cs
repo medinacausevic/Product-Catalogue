a3314371fdaf639c3a79d45eb256b7f9048efb9c
medinacausevic <medina.causevic95@gmail.com>
2020-12-07 01:40:42 +0100

    Create a project and connect to the database

---
+++ ProductCatalogue/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductCatalogue.Models;
+
+namespace ProductCatalogue.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductEntities db = new ProductEntities();
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult GetData()
+        {
+            using (ProductEntities db = new ProductEntities())
+            {
+                List<Product> productList = db.Products.ToList<Product>();
+                return Json(new { data = productList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
